83ee2ee8386013e374ef513f0fa65cbf24241918
Basile <basile.parent@arvato.fr>
2019-07-22 16:36:10 +0200

    Log on ping

--- src/pages/serverError/ServerError.jsx
+++ src/pages/serverError/ServerError.jsx
@@ -4,48 +4,54 @@ import axios from "axios";
 
 class ServerError extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-			url: props.url
-		};
-    }
-
-    _onInputChange = ({ target : { value }}) => {
-        const url = value + (!value.endsWith("/") ? "/" : "");
-        this.ping(url);
-        this.setState({ url });
+    this.state = {
+      value: props.url,
+      url: props.url
     };
+  }
 
-    ping = (url) => {
-		axios.get(url + "ping").then((response) => {
-		    console.log(response);
-		    if (response.status === 200) {
-				localStorage.setItem("serverUrl", url);
-				window.location.reload(false);
-            }
-		});
-    };
+  _onInputChange = ({target: {value}}) => {
+    this.ping(value);
+    this.setState({url: value});
+  };
 
-    render() {
-        const { url } = this.state;
+  ping = (url) => {
+    const urlPing = url + (!url.endsWith("/") ? "/" : "") + "ping";
+    axios.get(urlPing)
+      .then((response) => {
+        console.debug("PING", response);
+        if (response.status === 200) {
+          localStorage.setItem("serverUrl", url);
+          window.location.reload(false);
+        }
+      })
+      .catch(err => {
+        console.error("Error while pinging server on " + urlPing, err);
+      });
+  };
 
-        return (
-            <div id={"serverError"}>
-                <h1>Erreur lors de la requête du serveur</h1>
-                <p style={{ color : "red", fontWeight : "bold"}}>Cette erreur ne fait pas partie du jeu ! Contactez le MJ pour régler le problème.</p>
-                <div>
-                    <label htmlFor={"serverUrl"}>Url du serveur</label>
-                    <input id={"serverUrl"} type={"text"} value={url} onChange={this._onInputChange} />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const {url} = this.state;
+
+    return (
+      <div id={"serverError"}>
+        <h1>Erreur lors de la requête du serveur</h1>
+        <p style={{color: "red", fontWeight: "bold"}}>Cette erreur ne fait pas partie du jeu ! Contactez le MJ pour
+          régler le problème.</p>
+        <div>
+          <label htmlFor={"serverUrl"}>Url du serveur</label>
+          <input id={"serverUrl"} type={"text"} value={url} onChange={this._onInputChange}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 ServerError.propTypes = {
-    url : PropTypes.string.isRequired
+  url: PropTypes.string.isRequired
 };
 
 export default ServerError;
